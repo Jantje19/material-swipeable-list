1c5bcd5df08e561efc0cb089f72b61371b560e02
Jantje19 <Jantje19@users.noreply.github.com>
2021-01-15 17:08:44 +0100

    Updates

---
+++ precache-manifest.669e151dbd3f19ab0e7daceb4a158edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e16f7a9791738038ef46712d390146",
+    "url": "./index.html"
+  },
+  {
+    "revision": "100a7587efb8b558878d",
+    "url": "./static/js/2.cdb4c15d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.cdb4c15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2376fd86c89f5a85653",
+    "url": "./static/js/main.5fbbfb95.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.5fbbfb95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fb36dc1ba4f1018704",
+    "url": "./static/js/runtime-main.1a66a35d.js"
+  }
+]);
